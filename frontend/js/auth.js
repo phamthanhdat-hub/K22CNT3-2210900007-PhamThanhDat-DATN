7c67deae2862ff02935b6b1433320bf738288f52
PTD <thanhdat160924@gmail.com>
2026-01-27 08:01:50 +0700

    DATN

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,7 +1,7 @@
 function login(email, matKhau) {
     return fetch("http://127.0.0.1:5000/api/auth/login", {
         "email": "admin@babycutie.com",
-  "matKhau": "admin123",
+        "matKhau": "admin123",
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, matKhau })
